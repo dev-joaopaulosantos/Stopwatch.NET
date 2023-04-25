c31a8317e30fd9dec7fa0722d8b11ae26a5c1ef2
Joao Paulo Santos <joao.p.santos.silva22@gmail.com>
2023-04-25 16:51:31 -0300

    fear: função Start criada

--- Program.cs
+++ Program.cs
@@ -2,6 +2,21 @@
 {
     private static void Main(string[] args)
     {
-        
+        Console.Clear();
+
+        Start();
+    }
+
+    static void Start(){
+        int time = 10;
+        int currentTime = 0;
+
+        while (currentTime != time)
+        {
+            Console.Clear();
+            currentTime++;
+            Console.WriteLine(currentTime);
+            Thread.Sleep(1000);
+        }
     }
 }
