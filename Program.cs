120764d6d70cd01562c9cb090624438602544fde
Joao Paulo Santos <joao.p.santos.silva22@gmail.com>
2023-04-25 17:51:34 -0300

    feat: Menu finalizado

--- Program.cs
+++ Program.cs
@@ -7,20 +7,40 @@
         Menu();
     }
 
-    static void Menu(){
+    static void Menu()
+    {
         Console.WriteLine("S = Segundo");
         Console.WriteLine("M = Minuto");
         Console.WriteLine("0 = Sair");
-        Console.WriteLine("Quanto tempo deseja conttar? ");
+        Console.Write("Quanto tempo deseja conttar? ");
+
+        string data = Console.ReadLine().ToLower();
+        int multiplier = 1;
+
+        char type = char.Parse(data.Substring(data.Length - 1, 1));
+        int time = int.Parse(data.Substring(0, data.Length - 1));
+
+        if (type == 'm')
+        {
+            multiplier = 60;
+        }
+        if (time == 0)
+        {
+            System.Environment.Exit(0);
+        }
+
+        Start(time * multiplier);
+
     }
 
-    static void Start(int time){
-        // int time = 10;
+    static void Start(int time)
+    {
         int currentTime = 0;
 
         while (currentTime != time)
         {
             Console.Clear();
+
             currentTime++;
             Console.WriteLine(currentTime);
             Thread.Sleep(1000);
@@ -29,5 +49,6 @@
         Console.Clear();
         Console.WriteLine("Stopwatch finalizado!");
         Thread.Sleep(2500);
+        Menu();
     }
 }
